8286e82c82457a22acc9039f9feb2149c8f10e9d
dukhyun <dackhean@gmail.com>
2016-09-21 04:24:46 +0900

    js 파일 이동

--- assets/header.php
+++ assets/header.php
@@ -1,22 +1,12 @@
-<?php
-// include_once $root.'/../include/mysqli_function.php';
-?>
 <header id="header">
 	<div id="title">
 		<h1>portfolio</h1>
 	</div>
 	<nav id="nav">
 		<ul>
-			<li><a href="/" class="active">about</a></li>
-			<li><a href="/demo/">demo</a></li>
-			<li><a href="/study/">study</a></li>
+			<li><a href="" class="active">about</a></li>
+			<li><a href="">demo</a></li>
+			<li><a href="">study</a></li>
 		</ul>
 	</nav>
 </header>
-<!-- Scripts -->
-<script src="/assets/js/jquery.min.js"></script>
-<script src="/assets/js/jquery.scrollzer.min.js"></script>
-<script src="/assets/js/jquery.scrolly.min.js"></script>
-<script src="/assets/js/skel.min.js"></script>
-<script src="/assets/js/util.js"></script>
-<script src="/assets/js/main.js"></script>
