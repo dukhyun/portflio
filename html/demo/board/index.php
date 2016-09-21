f5647e1e3902fc98b48d0b0264df5bad47307e90
dukhyun <dackhean@gmail.com>
2016-09-21 09:08:32 +0900

    test

--- html/demo/board/index.php
+++ html/demo/board/index.php
@@ -12,18 +12,6 @@
 $root_url = '../..';
 include_once $root_url.'/../assets/db_function.php';
 $dbh = db_connect($root_url);
-
-$stmt = $dbh->prepare('UPDATE "Status" SET name = :update_name WHERE name = :search_name');
-$stmt->bindValue(':search_name', 'open');
-$stmt->bindParam(':update_name', $name, PDO::PARAM_STR);
-$name = 'public';
-$stmt->execute();
-
-$stmt = $dbh->query('SELECT * FROM "Status"');
-
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-	echo $row['id'].' '.$row['name'].'<br>';
-}
 ?>
 
 </body>
---
+++ html/test/google_code.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<section>
+	<h3>google-code-prettify</h3>
+	<pre class="prettyprint linenums">
+&lt;div class="col-lg-12"&gt;
+&nbsp;&nbsp;&lt;p&gt;Lorem ipsum dolor sit amet, consetetur sadipscing elitr..&lt;/p&gt;
+&nbsp;&nbsp;&lt;p&gt;Lorem ipsum dolor sit amet, consetetur sadipscing elitr..&lt;/p&gt;
+&nbsp;&nbsp;&lt;p&gt;Lorem ipsum dolor sit amet, consetetur sadipscing elitr..&lt;/p&gt;
+&lt;/div&gt;</pre>
+	<script src="/assets/js/google-code-prettify/run_prettify.js"></script>
+</section>
+
+
+
+</body>
+</html>
