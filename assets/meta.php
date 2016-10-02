d65d203a4c4b576be8c2a8b32328b155e806f6a8
dukhyun <dackhean@gmail.com>
2016-10-02 12:40:49 +0900

    http -> https 전환 (javascript)

--- assets/meta.php
+++ assets/meta.php
@@ -2,6 +2,14 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Dukhyun Portfolio Site</title>
+<!-- https -->
+<script type="text/javascript">
+if (documnet.location.host !== 'localhost' || documnet.location.host !== 'pf.dukhyun') {
+	if (document.location.protocol == 'http:') {
+		document.location.href = document.location.href.replace('http:', 'https:');
+	}
+}
+</script>
 <!-- include libraries(jQuery, bootstrap) -->
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
